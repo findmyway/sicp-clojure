a7ec20d9e07bbdde8f10310b9975d5fd5500afed
TianJun <find_my_way@foxmail.com>
2016-01-21 12:38:08 +0800

    update license

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject sicp-clojure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:author "TianJun"
+            :email "tianjun@tianjun.me"
+            :name "MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :year 2016
+            :key "mit"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-time "0.11.0"]])
